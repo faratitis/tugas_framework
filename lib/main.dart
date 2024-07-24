a76d2b4ff8bf5a74e7863f2c2d2e76781c3eb3bb
unknown <farahtitis@gmail.com>
2024-07-24 22:46:30 +0800

    ubah3

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,18 @@ import 'package:inventory_app/views/home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey:
+          'AIzaSyARy85uW_sio4sJUocdDxit3YNTmTL6FOI', // Ganti dengan apiKey dari Firebase Console
+      appId:
+          '1:741292122291:android:f19c42072716c5248b90c6', // Ganti dengan appId dari Firebase Console
+      messagingSenderId:
+          '741292122291', // Ganti dengan messagingSenderId dari Firebase Console
+      projectId:
+          'inventory-app-30d93', // Ganti dengan projectId dari Firebase Console
+    ),
+  );
   runApp(const MyApp());
 }
 
